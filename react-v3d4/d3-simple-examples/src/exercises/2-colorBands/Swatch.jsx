9230f244b0ef19d1d398479fe07900bfac34755d
Rémy Sabliet <sabliet@gmail.com>
2024-01-20 09:04:23 +0900

    cleaning up repository

--- react-v3d4/d3-simple-examples/src/exercises/2-colorBands/Swatch.jsx
+++
@@ -1,4 +0,0 @@
-export const Swatch = ({ color, width, x, y }) => {
-  console.log('Swatch props', color, width, x, y )
-  return <rect width={width} height='20' x={x} y={y} style={{ fill: color }} />;
-};
--- react-v3d4/d3-simple-examples/src/exercises/3-averageH1B-salaries/components/Meta/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Description } from './Description';
-export { default as GraphDescription } from './GraphDescription';
-export { default as Title } from './Title';
-
-
-
